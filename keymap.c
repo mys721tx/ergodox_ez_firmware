cd0f1ad34256db9eff8edd4b1a05412fd6e5be6e
Yishen Miao <mys721tx@gmail.com>
2020-01-10 22:47:54 -0800

    Adjusting del
    Adjusts del key.

--- keymap.c
+++ keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Keymap 0: Basic layer
     [BASE] = LAYOUT_ergodox(
         // Left hand
-        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_BSPACE,
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_NO,
         LCTL_T(KC_ESCAPE), KC_A, KC_S, KC_D, KC_F, KC_G,
         KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO,
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(FUNK),
         KC_SPACE, KC_ENTER, TG(NUMC),
         // Right hand
-        KC_NO, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPACE,
+        KC_DEL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPACE,
         KC_UP, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLASH,
         KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_ENTER,
         KC_DOWN, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT,
